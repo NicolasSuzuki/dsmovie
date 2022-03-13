b35b2a46bfe9a9942e8d8fe3321094c5adce6982
Nicolas Suzuki <nicolas.suzuki@gmail.com>
2022-03-13 18:48:23 -0300

    NavBar

---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,19 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+
+const Navbar = () => (
+    <header>
+        <nav className="container">
+            <div className="dsmovie-nav-content">
+                <h1>DSMovie</h1>
+                <a href="https://github.com/NicolasSuzuki" target="_blank" rel="noreferrer">
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/NicolasSuzuki</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+)
+
+export default Navbar;
