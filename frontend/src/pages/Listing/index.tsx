3ac2f9f5e46f6afdc571247d3ebeb3b61e95870a
Nicolas Suzuki <nicolas.suzuki@gmail.com>
2022-03-13 22:25:54 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -7,7 +7,7 @@ const Listing = () => {
             <Pagination />
             <div className='container'>
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
                 </div>
